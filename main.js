2c3a66e83fe626fa830afc21754e76b30fdbc306
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-05-23 18:06:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ global.loadDatabase = async function loadDatabase() {
 }
 loadDatabase()
 
-global.authFile = `${opts._[0] || 'session'}.data.json`
+global.authFile = `${opts._[0] || 'session'}.json`
 const {
 	state,
 	saveState
